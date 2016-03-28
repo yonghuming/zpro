8d75a0bf2571fbf8bc663db3a8206361dc72fe7d
pydo <964683112@qq.com>
2016-03-28 22:39:40 +0800

    修正index控制器显示请假列表逻辑

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -16,7 +16,7 @@ class IndexController extends Controller {
 public function agree($id,$uid) {
         $kaoqin = M('kaoqin');
         $data['id'] = $id;
-        $data['uid'] = $id;
+        $data['uid'] = $uid;
         $kaoqin->flag = 1;
         if($kaoqin->where($data)->save()){
           $this->success('sueccess',U('Index/index'));  
@@ -27,7 +27,7 @@ public function agree($id,$uid) {
     public function disagree($id,$uid) {
         $kaoqin = M('kaoqin');
         $data['id'] = $id;
-        $data['uid'] = $id;
+        $data['uid'] = $uid;
         $kaoqin->flag = 0;
         if($kaoqin->where($data)->save()){
             $this->success('sueccess',U('Index/index'));
@@ -40,7 +40,7 @@ public function agree($id,$uid) {
         #获取所有申请列表，按照时间顺序
     	$kaoqin = D('kaoqin');
     	$list = $kaoqin->select();
-    	$list=$kaoqin->join('think_members ON think_members.id = think_kaoqin.uid','left')->select();
+    	$list=$kaoqin->join('think_membes ON think_member.uid = think_kaoqin.uid','left')->select();
     	$this->assign('list',$list);
     	$this->display();
         #可以支持不同分类视图
