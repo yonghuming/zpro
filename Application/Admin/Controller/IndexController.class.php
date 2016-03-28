0d831752f7928fd50fc2e5ebcf5d832b20a78c71
pydo <964683112@qq.com>
2016-03-28 22:27:09 +0800

    数据库表混乱

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -6,15 +6,17 @@ use Think\Auth;
 class IndexController extends Controller {
 	public function _initialize(){
        # session(array('expire'=>15));
-       
+	    
+	    
+	   
           if (!isset($_SESSION['admin-username'])){
             $this->error('请先登录',U('Member/login'));
         }
       }
-public function agree($id,$uid) {
+public function agree($id,$id) {
         $kaoqin = M('kaoqin');
         $data['id'] = $id;
-        $data['uid'] = $uid;
+        $data['id'] = $id;
         $kaoqin->flag = 1;
         if($kaoqin->where($data)->save()){
           $this->success('sueccess',U('Index/index'));  
@@ -22,10 +24,10 @@ public function agree($id,$uid) {
             $this->error($kaoqin->getError());
         }
     }
-    public function disagree($id,$uid) {
+    public function disagree($id,$id) {
         $kaoqin = M('kaoqin');
         $data['id'] = $id;
-        $data['uid'] = $uid;
+        $data['id'] = $id;
         $kaoqin->flag = 0;
         if($kaoqin->where($data)->save()){
             $this->success('sueccess',U('Index/index'));
@@ -38,7 +40,7 @@ public function agree($id,$uid) {
         #获取所有申请列表，按照时间顺序
     	$kaoqin = D('kaoqin');
     	$list = $kaoqin->select();
-    	$list=$kaoqin->join('think_member ON think_member.uid = think_kaoqin.uid','left')->select();
+    	$list=$kaoqin->join('think_members ON think_members.id = think_kaoqin.uid','left')->select();
     	$this->assign('list',$list);
     	$this->display();
         #可以支持不同分类视图
--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -11,6 +11,14 @@ return array(
 	'DB_CHARSET'=>'utf8',// 数据库字符集
 	'LAYOUT_ON'=>true,
 	'LAYOUT_NAME'=>'layout',
+    'AUTH_CONFIG'=>array(
+        'AUTH_ON' => true, //认证开关
+        'AUTH_TYPE' => 1, // 认证方式，1为时时认证；2为登录认证。
+        'AUTH_GROUP' => 'think_auth_group', //用户组数据表名
+        'AUTH_GROUP_ACCESS' => 'think_auth_group_access', //用户组明细表
+        'AUTH_RULE' => 'think_auth_rule', //权限规则表
+        'AUTH_USER' => 'think_members'//用户信息表
+    ),
 	
 	#'SHOW_PAGE_TRACE'=>True,
 	'SESSION_OPTIONS'         =>  array(
