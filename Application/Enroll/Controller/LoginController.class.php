b69457fca9b599b84fc605b80b1cd5dd1271e66d
pydo <964683112@qq.com>
2016-03-30 20:34:13 +0800

    登录后重新登录

--- Application/Enroll/Controller/LoginController.class.php
+++ Application/Enroll/Controller/LoginController.class.php
@@ -75,21 +75,7 @@ class LoginController extends Controller {
         
             $user = D('members');
           
-            // 自动验证 创建数据集
-//             $rules = array(
-                 
-//                 array('username', 'require', '用户名不能为空！',1), //默认情况下用正则进行验证
-            
-//                 // 正则验证密码 [需包含字母数字以及@*#中的一种,长度为6-22位]
-//                 array('passwd', '/^([a-zA-Z0-9@*#]{6,22})$/', '密码格式不正确,请重新输入！', 1),
-//                 array('repasswd', 'passwd', '确认密码不正确', 1, 'confirm'), // 验证确认密码是否和密码一致
-//                 array('email', 'email', '邮箱格式不正确'), // 内置正则验证邮箱格式
-//                 array('mobile', '/^1[34578]\d{9}$/', '手机号码格式不正确', 1), // 正则表达式验证手机号码
-//                 array('verify', 'verify_check', '验证码错误', 1, 'function'), // 判断验证码是否正确
-//                 //array('agree', 'is_agree', '请先同意网站安全协议！', 1, 'callback'), // 判断是否勾选网站安全协议
-//                 array('agree', 'require', '请先同意网站安全协议！', 1), // 判断是否勾选网站安全协议
-//             );
-            #if (!$data = $user->validate($rules)->create()) {
+
                 if (!$data = $user->create()) {
                 
                 // 防止输出中文乱码
@@ -140,11 +126,11 @@ class LoginController extends Controller {
         $verify = new \Think\Verify();
 
         // 配置验证码参数
-        $verify->fontSize = 14;     // 验证码字体大小
+        $verify->fontSize = 20;     // 验证码字体大小
         $verify->length = 4;        // 验证码位数
         $verify->imageH = 34;       // 验证码高度
-        $verify->useImgBg = true;   // 开启验证码背景
-        $verify->useNoise = false;  // 关闭验证码干扰杂点
+        $verify->useImgBg = false;   // 开启验证码背景
+        $verify->useNoise = true;  // 关闭验证码干扰杂点
         $verify->entry();
     }
 }
