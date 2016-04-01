1c7c150b095672c61de6336f4a41ebd2e4f91e3f
pydo <964683112@qq.com>
2016-04-01 12:54:25 +0800

    完善报名功能
    修改界面，完善增删改查，增加初中成绩排名和获奖项目

--- Application/Enroll/Conf/config.php
+++ Application/Enroll/Conf/config.php
@@ -3,14 +3,7 @@ return array(
 	//'配置项'=>'配置值'
     'SHOW_PAGE_TRACE'=> True,
    # 'SESSION_PREFIX'=>'enroll',
-    'HTML_CACHE_ON'     =>    false, // 开启静态缓存
-    'HTML_CACHE_TIME'   =>    60,   // 全局静态缓存有效期（秒）
-    'TMPL_CACHE_ON'   => true,  // 默认开启模板编译缓存 false 的话每次都重新编译模板
- 
-'ACTION_CACHE_ON'  => false,  // 默认关闭Action 缓存
- 
 
- 
  
     
 );
--- Application/Enroll/Controller/IndexController.class.php
+++ Application/Enroll/Controller/IndexController.class.php
@@ -8,6 +8,7 @@ class IndexController extends Controller
 
     public function _initialize()
     {
+
         if (null == session('enrolluid')) {
             
             $this->error('请先登录', U('Login/login'));
@@ -19,6 +20,31 @@ class IndexController extends Controller
             $this->assign('pic_url',$pic_url);
         }
         $this->display();
+    }
+    /**
+        判断有无权限
+        判断是否可以编辑
+    */
+    public function edit(){
+        $enroll = D('enroll');
+            $result = $enroll->where('uid = ' . $_SESSION['enrolluid'])->find();
+           trace($_SESSION['enrolluid']);
+           #如果是报名了，显示报名表
+           #如果没有报名，提示报名
+
+            if ($result && $result['id_number'] != null) {
+                 $this->assign('enrolled',1);
+                foreach ($result as $k => $v) {
+                    trace($k.'=>'.$v);
+                    $this->assign("$k", $v);
+
+                }                
+                    $this->display();
+            }else{
+                    $this->display();
+                    
+                }
+        
     }
     public function upload(){
         $upload = new \Think\Upload();// 实例化上传类
@@ -34,13 +60,15 @@ class IndexController extends Controller
         }else{// 上传成功
             $enroll = D('enroll');
            
-            $pic_url = 'Uploads/'.$info['pic_url']['savepath'].$info['pic_url']['savename'];
-            var_dump($pic_url);
-            exit();
-            $this->assgin('pic_url',$pic_url);
-            $enroll->where('uid = '.session('enrolluid'))->save();
+            #$pic_url = $upload->savePath.$info[0]['savename'];
+            $pic_url = $upload->rootPath.date('Y-m-d').'/'.$info['pic_url']['savename'];
+           
+            $this->assign('pic_url',$pic_url);
+            $data['pic_url'] = $pic_url;
+           
+            $enroll->where('uid = '.session('enrolluid'))->save($data);
             
-            $this->success('上传成功！');
+            $this->success('上传成功！','',1);
         }
     }
     public function index()
@@ -51,20 +79,27 @@ class IndexController extends Controller
             // 将所有字段读出并放到表格里
             $enroll = D('enroll');
             $result = $enroll->where('uid = ' . $_SESSION['enrolluid'])->find();
-            trace($_SESSION['enrolluid']);
-            trace($result);
-            $this->assign('edit',1);
+           trace($_SESSION['enrolluid']);
+           #如果是报名了，显示报名表
+           #如果没有报名，提示报名
+
             if ($result && $result['id_number'] != null) {
+                 $this->assign('enrolled',1);
                 foreach ($result as $k => $v) {
                     trace($k.'=>'.$v);
                     $this->assign("$k", $v);
+
+                }                
+                    $this->display();
+            }else{
+                    $this->display();
+                    
                 }
-            }
             
             // 不是提交，显示
-            $this->display();
+            
         } else {
-            session('edit',0);
+          
                 $enroll = D('members');
                 $result = $enroll->where('id = ' . $_SESSION['enrolluid'])->find();
                 // 还差个人荣誉
@@ -74,7 +109,7 @@ class IndexController extends Controller
            
             // 逻辑上是，从members用户表读取用户名和手机号mobile来填充到报名表
            
-                $this->assign('edit',0);
+              
            $this->display();
         }
     }
@@ -90,6 +125,20 @@ class IndexController extends Controller
                 $enroll->uid = $_SESSION['enrolluid'];
                 $enroll->add();
                 $this->success('恭喜您，报名成功', U('Index/index'));
+            } else {
+                $this->error($enroll->getError());
+            }
+        }
+    }
+    public function update(){
+        if (! IS_POST) {
+            $this->error();
+        } else {
+            $enroll = D('enroll'); 
+            if ($enroll->create()) {
+                $uid  = $_SESSION['enrolluid'];
+                $enroll->where('uid = '.$uid)->save();
+                $this->success('修改报名信息成功', U('Index/index'));
             } else {
                 $this->error($enroll->getError());
             }
