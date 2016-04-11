94a12a3e88ecfe5721a1c17a56c2a8144d19e970
pydo <964683112@qq.com>
2016-04-11 16:37:10 +0800

    最后一次修改

--- Application/Enroll/Controller/LoginController.class.php
+++ Application/Enroll/Controller/LoginController.class.php
@@ -144,13 +144,15 @@ class LoginController extends Controller
     {
         // 实例化Verify对象
         $verify = new \Think\Verify();
-        
+        $verify->codeSet = '0123456789'; 
         // 配置验证码参数
        /* $verify->fontSize = 15; // 验证码字体大小
         $verify->length = 4; // 验证码位数
         $verify->imageH = 34; // 验证码高度
         $verify->useImgBg = false; // 开启验证码背景
         $verify->useNoise = true; // 关闭验证码干扰杂点*/
+        $verify->useNoise = false;
+
         $verify->entry();
     }
 }
