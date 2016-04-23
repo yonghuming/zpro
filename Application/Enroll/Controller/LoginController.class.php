bb5846b090c3fa86af454214e6fc7ed382a44b09
pydo <964683112@qq.com>
2016-04-23 16:55:44 +0800

    222

--- Application/Enroll/Controller/LoginController.class.php
+++ Application/Enroll/Controller/LoginController.class.php
@@ -36,7 +36,9 @@ class LoginController extends Controller
     
     public function query()
     {
-    
+        if (strtotime(date('Y-m-d H:i')) < strtotime('2016-4-23 06:00')) {
+            $this->error('亲，未到查询时间哦，(●—●)');
+        }
         // 判断提交方式
         if (IS_POST) {
             // 实例化Login对象
